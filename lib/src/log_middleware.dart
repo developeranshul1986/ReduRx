8892d7c64564739b3a32bdf2c923e1a9ed47870a
Wes <wes@cosmicpangolin.com>
2018-12-24 13:04:44 -0700

    /// Nomi Modifications: /// - Changed several functions to take/return Store instead of state T, /// propagating Store through to actions. This enables some powerful features /// since you can now dispatch from middleware/actions: /// chaining actions with middleware and dispatching nested actions!! /// - Deleted the Computation typedef 'cause it's just redundant /// - Removed Store(state) variable assignments in Store.dispatch, saving memory! /// (I think?)

--- lib/src/log_middleware.dart
+++ lib/src/log_middleware.dart
@@ -4,14 +4,14 @@ import '../redurx.dart';
 class LogMiddleware<T> extends Middleware<T> {
   /// Prints before the Action reducer call.
   @override
-  T beforeAction(action, state) {
+  Store<T> beforeAction(action, state) {
     print('Before action: ${action.runtimeType}: $state');
     return super.beforeAction(action, state);
   }
 
   /// Prints after the Action reducer call.
   @override
-  T afterAction(action, state) {
+  Store<T> afterAction(action, state) {
     print('After action: ${action.runtimeType}: $state');
     return super.afterAction(action, state);
   }
