6597d25e4e69b2e2ddf0dd34354f381996036114
Wes <wes@cosmicpangolin.com>
2018-12-25 00:13:16 -0700

    ///  A thin layer of a Redux-based state manager on top of RxDart. /// /// Nomi Modifications: /// - Implemented Store<T> as a Middleware parameter, so Store.dispatch can be ///   called from Middleware to handle side effects/nested actions. Two states ///   get added to store per action.  Tried many ways of passing store/state ///   around, but this was the only way that works. ///   cleanly given Flutter and Built_Value limitations. /// - Deleted the Computation typedef 'cause it's just redundant /// - Removed the second 'beforeAction' Middleware call for AsyncActions /// - Removed Store(state) variable assignments in Store.dispatch, saving memory! /// (I think?)

--- lib/src/log_middleware.dart
+++ lib/src/log_middleware.dart
@@ -4,15 +4,15 @@ import '../redurx.dart';
 class LogMiddleware<T> extends Middleware<T> {
   /// Prints before the Action reducer call.
   @override
-  T beforeAction(action, store, state) {
-    print('Before action: ${action.runtimeType}: $state');
-    return super.beforeAction(action, store, state);
+  T beforeAction(action, store) {
+    print('Before action: ${action.runtimeType}: $store.state');
+    return super.beforeAction(action, store);
   }
 
   /// Prints after the Action reducer call.
   @override
-  T afterAction(action, store, state) {
-    print('After action: ${action.runtimeType}: $state');
-    return super.afterAction(action, store, state);
+  T afterAction(action, store) {
+    print('After action: ${action.runtimeType}: $store.state');
+    return super.afterAction(action, store);
   }
 }
